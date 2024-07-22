4c2242ff4a20f005f3e28bb56c98406bc2bea5bc
TerFree70 <terfree@gmail.com>
2024-07-22 15:58:03 +0300

    fix bug

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -2,10 +2,12 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '7466c775e142a81f5c61f49eefe1f872'
-HEADER = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
 TRAINER_ID = '6387'
 
 def test_status_code():
     response = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
     assert response.status_code == 200
+
+def test_trainer_name():
+    response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_get.json()['data'][0]['trainer_name'] == 'terfree'
